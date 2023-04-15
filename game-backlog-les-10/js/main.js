ad0c3faef3d06cb6cb2f34bb21dfd25a1667635b
Uday-Singh G <91188742+Uday-Singh1@users.noreply.github.com>
2023-04-15 23:47:14 +0200

    Backlog-changes

---
+++ game-backlog-les-10/js/main.js
@@ -0,0 +1,87 @@
+class Api {
+    data = null;
+    async getData() {
+        await fetch("../data/games.json").then(response => {
+            return response.json();
+        }).then(newData => {
+            this.data = newData.games; //newData["games"]
+        }); 
+    }
+}
+
+class Filter {
+    filteredResult = [];
+
+    filter(platform,data) {
+        for(let i = 0; i < data.length; i++) {
+            if(data[i].platform === platform) {
+                this.filteredResult.push(data[i]);
+            }
+        }
+    }
+    randomFromResult() {
+        let randomNumber = Math.floor(Math.random() * this.filteredResult.length); // 0, 1, 2
+        return this.filteredResult[randomNumber];
+    }
+}
+
+class URLScraper{
+    currentURL;
+    platform;
+    pretty;
+    constructor() {
+        this.currentURL = window.location.href;
+
+    }
+    getDataFromURL() {
+        this.platform = this.currentURL.split("platform=")[1];
+        this.pretty = new PrettyPlatform(this.platform);
+        this.platform = this.pretty.platform;
+    }
+}
+
+class PrettyPlatform {
+    platform;
+
+    constructor(platform) {
+        this.platform = platform;
+        this.platformToUpperCase();
+        this.removeSpaces();
+    }
+
+    platformToUpperCase() {
+        this.platform = this.platform.toUpperCase();
+    }
+
+    removeSpaces() {
+        this.platform = this.platform.replaceAll(" ", "");
+        this.platform = this.platform.replaceAll("%20","");
+    }
+
+}
+
+class App {
+    api;
+    filter;
+    urlScraper;
+    constructor() {
+        this.api = new Api(); // changed from Api to GetData
+        this.filter = new Filter();
+        this.urlScraper = new URLScraper();
+
+        this.urlScraper.getDataFromURL();
+        
+        this.api.getData().then(
+            () => {
+                this.filter.filter(this.urlScraper.platform,this.api.data);
+                let randomResult = this.filter.randomFromResult();
+                console.log(randomResult);
+            }
+         );
+    }
+}
+
+const app = new App();
+
+
+//video 6
